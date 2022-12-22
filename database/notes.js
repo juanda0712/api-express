7481b28b4fe8d4ebd43f65226aa55058c363c137
Juan Rodriguez <juanrm071299@gmail.com>
2022-12-21 18:08:36 -0600

    upload api

---
+++ database/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    id: 1,
+    content: 'Tengo que ir a la universidad',
+    date: '2020-05-41811:30:31.1245',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Tengo que ir a la universidad',
+    date: '2020-05-41811:30:31.1245',
+    important: true
+  },
+  {
+    id: 3,
+    content: 'Tengo que ir a la universidad',
+    date: '2020-05-41811:30:31.1245',
+    important: true
+  },
+  {
+    id: 4,
+    content: 'Tengo que ir a la universidad',
+    date: '2020-05-41811:30:31.1245',
+    important: true
+  }
+]
+
+module.exports = notes
---
+++ index.js
@@ -0,0 +1,66 @@
+let notes = require('./database/notes')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors)
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Bienvenido a mi nueva API')
+})
+
+app.get('/api/notes', (req, res) => {
+  console.log(notes)
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params
+  const note = notes.find((note) => {
+    return note.id === Number(id)
+  })
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params
+  notes = notes.filter((note) => {
+    return note.id !== Number(id)
+  })
+  res.status(204).end()
+})
+
+app.post('/api/notes', (req, res) => {
+  const note = req.body
+
+  if (!note || !note.content) {
+    return res.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
+
+  const ids = notes.map((note) => note.id)
+  const max = Math.max(...ids)
+
+  const newNote = {
+    id: max + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+
+  notes.push(newNote)
+  res.status(201).end()
+})
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Not Found'
+  })
+})
+
+app.listen(3005)
