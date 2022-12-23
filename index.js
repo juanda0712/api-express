da593449fce8e45d0901e53fe23903b93d76edf0
Juan Rodriguez <juanrm071299@gmail.com>
2022-12-22 19:12:08 -0600

    update CORS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Bienvenido a mi nueva API')
